d882d4d2422b0d9b26ed5f2bd495ec608f9ba512
zhouce <470957509@qq.com>
2023-07-08 20:18:48 +0800

    第二次更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -10,5 +10,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
